f5efa60d7003fde4faacc93824b8c82d31f214ad
AMARESH PRASAD <141488853+devAmaresh@users.noreply.github.com>
2023-12-09 22:45:19 +0530

    Update dbconnection.php

--- result_searchengine/admin/dbconnection.php
+++ result_searchengine/admin/dbconnection.php
@@ -2,7 +2,7 @@
 $db_host="localhost";
 $db_user="root";
 $db_pass="";
-$db_name="iSchool";
+$db_name="ischool";
 $db_port="3307";
 
 $conn=mysqli_connect($db_host,$db_user,$db_pass,$db_name,$db_port);
